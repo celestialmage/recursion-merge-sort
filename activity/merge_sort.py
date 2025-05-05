a666053cacdb81bc0afdd8816587c6dc57a54b94
Max <imax.mr@gmail.com>
2025-05-05 11:24:51 -0700

    completed merge_sort

--- activity/merge_sort.py
+++ activity/merge_sort.py
@@ -1,4 +1,5 @@
 from .mergelib import merge_sorted
+from math import ceil
 
 # Implement merge_sort
 # merge_sorted is a helper function that merges 2
@@ -6,4 +7,13 @@ from .mergelib import merge_sorted
 # with respect to the combined lengths of the lists.
 
 def merge_sort(data):
-    pass
+    data_length = len(data)
+
+    if data_length == 1:
+        return data
+    else:
+        split_by = ceil(data_length / 2)
+        list1= merge_sort(data[:split_by])
+        list2= merge_sort(data[split_by:])
+
+        return merge_sorted(list1, list2)
